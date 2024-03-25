5762f11e23897ccf9f97ae03fa11ad51e5bbbe47
Jake Soucy <113120544+FreePirat@users.noreply.github.com>
2024-03-25 16:12:31 -0300

    Finished The Database

--- ThemeTussleMoveList/ThemeTussleMoveList/Scripts/Tussle-Scripts.sql
+++ ThemeTussleMoveList/ThemeTussleMoveList/Scripts/Tussle-Scripts.sql
@@ -1,10 +1,11 @@
 USE master
-GO 
+GO
 
 IF EXISTS (SELECT * FROM sys.databases WHERE NAME = 'MOVELIST')
-
-BEGIN DROP Database MOVELIST
+BEGIN
+    DROP DATABASE MOVELIST
 END
+GO
 
 CREATE DATABASE MOVELIST
 GO
@@ -12,51 +13,59 @@ GO
 USE MOVELIST
 GO
 
-DROP TABLE IF EXISTS Character
+DROP TABLE IF EXISTS dbo.Character
 GO
 
-CREATE TABLE Character (
-	CharacterId		INT			 IDENTITY(1,1)	PRIMARY KEY,
-	CharacterName	VARCHAR(255) NOT NULL,
-	Health			INT			 NOT NULL,
-	JumpHeight		INT			 NOT NULL,
-	Description		VARCHAR(255) NULL
+CREATE TABLE dbo.Character (
+    CharacterId     INT          IDENTITY(1,1) PRIMARY KEY,
+    CharacterName   VARCHAR(255) NOT NULL,
+    Health          INT          NOT NULL,
+    JumpHeight      INT          NOT NULL,
+    Description     VARCHAR(255) NULL
 );
 
-DROP TABLE IF EXISTS Move
+DROP TABLE IF EXISTS dbo.Move
 GO
 
-CREATE TABLE Move(
-	MoveId			INT			IDENTITY(1,1)	PRIMARY KEY,
-	InputName		VARCHAR(10)	NOT NULL,
-	Damage			INT			NOT NULL,
-	StartupFrames	INT			NOT NULL,
-	ActiveFrames	INT			NOT NULL,
-	RecoveryFrames	INT			NOT NULL,
-	OnBlock			INT			NOT NULL,
-	HitBoxSize		INT			NOT NULL,
-	HitBoxPos		INT			NOT NULL,
-	HurtBoxSize		INT			NOT NULL,
-	HurtBoxPos		INT			NOT NULL,
-	Description		VARCHAR(255)NULL
+CREATE TABLE dbo.Move (
+    MoveId          INT          IDENTITY(1,1) PRIMARY KEY,
+    CharacterId     INT          NOT NULL,
+    InputName       VARCHAR(10)  NOT NULL,
+    Damage          INT          NOT NULL,
+    StartupFrames   INT          NOT NULL,
+    ActiveFrames    INT          NOT NULL,
+    RecoveryFrames  INT          NOT NULL,
+    OnBlock         INT          NOT NULL,
+    HitBoxSizeX     INT          NOT NULL,
+    HitBoxSizeY     INT          NOT NULL,
+    HitBoxPosX      INT          NOT NULL,
+    HitBoxPosY      INT          NOT NULL,
+    HurtBoxSizeX    INT          NOT NULL,
+    HurtBoxSizeY    INT          NOT NULL,
+    HurtBoxPosX     INT          NOT NULL,
+    HurtBoxPosY     INT          NOT NULL,
+    Description     VARCHAR(255) NULL
 );
 
-ALTER TABLE Move
-ADD CONSTRAINT FK_Move_Character FOREIGN KEY (CharacterId)
-REFERENCES Character(CharacterId);
+INSERT INTO dbo.Character (CharacterName, Health, JumpHeight, Description)
+VALUES ('Nina', 400, 600, 'A character with average speed good damage and tons of tools'),
+       ('Suzie', 320, 600, 'A fast and fragile character capable of mixing the opponent up quick'),
+       ('Leo', 450, 500, 'A mighty king huge in size and massive in damage.'),
+       ('Charlie', 400, 600, 'A jester with some RNG added into the mix');
 
-SET IDENTITY_INSERT Character ON  
-INSERT INTO Character (CharacterId,CharacterName,Health,JumpHeight,Description) VALUES(1,'Nina',400,600,'A character with average speed good damage and tons of tools')
-INSERT INTO Character (CharacterId,CharacterName,Health,JumpHeight,Description) VALUES(2,'Suzie',320,600,'A fast and fragile charater capable of mixing the opponent up quick')
-INSERT INTO Character (CharacterId,CharacterName,Health,JumpHeight,Description) VALUES(3,'Leo',450,500,'A mighty king huge in size and massive in damage.')
-INSERT INTO Character (CharacterId,CharacterName,Health,JumpHeight,Description) VALUES(4,'Charlie',400,600,'A jester with some rng added into the mix')
-SET IDENTITY_INSERT Character OFF
+INSERT INTO dbo.Move (CharacterId, InputName, Damage, StartupFrames, ActiveFrames, RecoveryFrames, OnBlock, HitBoxSizeX, HitBoxSizeY, HitBoxPosX, HitBoxPosY, HurtBoxSizeX, HurtBoxSizeY, HurtBoxPosX, HurtBoxPosY, Description)
+VALUES (1,'5L', 20, 5,   5, 7,   -2,     100, 50,    60, -30, 160, 370, -40, 0,  'An average light poke'),
+       (1,'5M', 38, 7,   8, 10,   1,     100, 70,    60,  90, 160, 370, -40, 0,  'An average medium poke'),
+       (1,'5H', 45, 12,  5, 28,  -15,    150, 100,   30, -30, 160, 370, -40, 0,  'An average heavy poke'),
+       (1,'2L', 18, 5,   4, 9,   -2,     90,  50,    60,  30, 160, 300, -15, 20, 'An average light poke(LOW BLOCK)'),
+       (1,'2M', 30, 8,   4, 21,  -10,    130, 55,    50, 130, 160, 300, -15, 20, 'An average medium poke(LOW BLOCK)'),
+       (1,'2H', 36, 12,  9, 22,  -14,    120, 90,    40,   0, 160, 370, -15, 20, 'An attack that sends the opponent flying upwards'),
+       (1,'6L', 28, 11,  6, 17,  -9,     160, 145,   30, -60, 160, 180, -40, 70, 'An average light anti-air'),
+       (1,'6M', 35, 20,  5, 7,    2,     105, 80,    70, -30, 160, 370, -40, 0,  'Nina flicks a coin to hit their opponent (HIGH BLOCK)'),
+       (1,'6H', 50, 17,  5, 15,  -6,     140, 150,   30, -30, 160, 370, -40, 0,  'A hard hititng finisher attack'),
+       (1,'8L', 20, 5,   5, 10,   0,     60,  65,    70,   0, 160, 300,   0, 0,  'An average light poke(HIGH BLOCK)'),
+       (1,'8M', 24, 8,   4, 10,   0,     70,  70,    50,  40, 160, 300,   0, 0,  'An average light poke(HIGH BLOCK)'),
+       (1,'8H', 38, 12,  4, 18,   0,     90,  105,   15,  15, 160, 300,   0, 0,  'An average light poke(HIGH BLOCK)');
 
-SELECT * FROM Character
-
-SET IDENTITY_INSERT Move ON  
-INSERT INTO Move (MoveId,InputName,Damage,StartupFrames,ActiveFrames,RecoveryFrames,OnBlock,HitBoxSizeX,HitBoxSizeY,HitBoxPosX,HitBoxPosY,HurtBoxSizeX,HurtBoxSizeY,HurtBoxPosX,HurtBoxPosY,Description)
-			VALUES(1,'5L',20,5,5,7,-2,100,50,60,-30,160,370,-40,0,'An average light poke')
-SET IDENTITY_INSERT Move OFF
-
-SELECT * FROM Move
+SELECT * FROM dbo.Character
+SELECT * FROM dbo.Move
